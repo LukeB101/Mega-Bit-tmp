d99a772087db2d9958588d9af90b2d3589aa1699
LukeB101 <luke_bussell@yahoo.co.uk>
2018-05-27 03:42:25 +0100

    Add files via upload

---
+++ DEMO/README.txt
@@ -0,0 +1,3 @@
+This folder contains the code for the working demonstrations of the modified DAL code.
+
+Demo1 is shown on YouTube here: https://youtu.be/Rcw2uC2LSDU
---
+++ DEMO/demo1-characters.cpp
@@ -0,0 +1,33 @@
+#include "MicroBit.h"
+
+MicroBit uBit;
+
+int main()
+{
+    uBit.init();
+
+	int number = 0;
+	for (int z = 0; z < 10; z++) {
+		number = z;
+		ManagedString t(number);
+		uBit.display.printChar(t.charAt(0), 600);
+	}
+
+	char lower[] = { "a" };
+	for (int z = 0; z < 5; z++) {
+		ManagedString t(lower);
+		uBit.display.printChar(t.charAt(0), 600);
+		lower[0] += 1;
+	}
+
+	char upper[] = { "A" };
+	for (int z = 0; z < 5; z++) {
+		ManagedString t(upper);
+		uBit.display.printChar(t.charAt(0), 600);
+		upper[0] += 1;
+	}
+
+    release_fiber();
+
+    return 0;
+}
